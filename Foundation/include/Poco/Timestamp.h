a3610f99ae167c0f8b6544fac769d8120094ee5d
Michael Forkey <michael.forkey@avt-usa.com>
2015-03-06 17:43:06 -0800

    Fixed YT-662 - mismatched POCO header & binary for Timestamp::fromEpochTime().

--- Foundation/include/Poco/Timestamp.h
+++ Foundation/include/Poco/Timestamp.h
@@ -128,7 +128,8 @@ public:
 		///
 		/// Same as epochMicroseconds().
 	
-	static Timestamp fromEpochTime(std::time_t t);
+	//Argument type changed to be compatible with ViM built with _USE_32BIT_TIME_T.
+	static Timestamp fromEpochTime(__time64_t t);
 		/// Creates a timestamp from a std::time_t.
 		
 	static Timestamp fromUtcTime(UtcTimeVal val);
